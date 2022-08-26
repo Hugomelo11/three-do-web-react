7a44ef8a94aa87b863a9ebad046276ba624ae7cb
Hugo Salles <salles.hugo05@gmail.com>
2022-08-26 12:05:36 -0400

    creating react app for three-do

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+export default function TodoList({ tasklist, setTasklist }) {
+  // call the api and use setTaskList to fill in state
+  useEffect(() => {
+    fetch('https://three-do-api-bc.web.app/tasks')
+     .then(results => results.json())
+     .then(setTasklist)
+     .catch(console.error);
+  }, [setTasklist]);
+  if (!tasklist) {
+    return <h2>No tasks to complete!</h2>;
+  }
+
+  return (
+    <ul>
+      {tasklist.map((task) => (
+        <li key={task.id}>{task.task}</li>
+      ))}
+    </ul>
+  );
+}
